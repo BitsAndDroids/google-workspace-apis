092d3de1b7e48a9a830f32f91405a7fa9d45a3db
Bits and Droids <info@BitsAndDroids.com>
2025-09-20 07:22:18 +0200

    added drive scopes

--- src/auth/scopes.rs
+++ src/auth/scopes.rs
@@ -17,12 +17,24 @@ pub enum Scope {
     MailModify,
     MailReadonly,
     MailMetadata,
+    //Drive
+    DriveAppfolder,
+    DriveInstall,
+    DriveFile,
+    DriveAppsReadonly,
+    Drive,
+    DriveReadonly,
+    DriveActivity,
+    DriveActivityReadonly,
+    DriveMeetReadonly,
+    DriveMetadata,
+    DriveMetadataReadonly,
+    DriveScripts,
 }
 
 impl Scope {
     pub fn as_str(&self) -> &str {
         match self {
-            //Calendar
             Scope::Calendar => "https://www.googleapis.com/auth/calendar",
             Scope::CalendarEventsReadonly => {
                 "https://www.googleapis.com/auth/calendar.events.readonly"
@@ -40,14 +52,31 @@ impl Scope {
             }
             Scope::CalendarReadOnly => "https://www.googleapis.com/auth/calendar.readonly",
             Scope::CalendarEvents => "https://www.googleapis.com/auth/calendar.events",
-            //Tasks
             Scope::TasksReadOnly => "https://www.googleapis.com/auth/tasks.readonly",
             Scope::Tasks => "https://www.googleapis.com/auth/tasks",
-            //GMAIL
             Scope::Mail => "https://mail.google.com",
             Scope::MailModify => "https://www.googleapis.com/auth/gmail.modify",
             Scope::MailReadonly => "https://www.googleapis.com/auth/gmail.readonly",
             Scope::MailMetadata => "https://www.googleapis.com/auth/gmail.metadata",
+            Scope::DriveAppfolder => {
+                "https://www.googleapis.com/auth/drive.appdata
+https://www.googleapis.com/auth/drive.appfolder"
+            }
+            Scope::DriveInstall => "https://www.googleapis.com/auth/drive.install",
+            Scope::DriveFile => "https://www.googleapis.com/auth/drive.file",
+            Scope::DriveAppsReadonly => "https://www.googleapis.com/auth/drive.apps.readonly",
+            Scope::Drive => "https://www.googleapis.com/auth/drive",
+            Scope::DriveReadonly => "https://www.googleapis.com/auth/drive.readonly",
+            Scope::DriveActivity => "https://www.googleapis.com/auth/drive.activity",
+            Scope::DriveActivityReadonly => {
+                "https://www.googleapis.com/auth/drive.activity.readonly"
+            }
+            Scope::DriveMeetReadonly => "https://www.googleapis.com/auth/drive.meet.readonly",
+            Scope::DriveMetadata => "https://www.googleapis.com/auth/drive.metadata",
+            Scope::DriveMetadataReadonly => {
+                "https://www.googleapis.com/auth/drive.metadata.readonly"
+            }
+            Scope::DriveScripts => "https://www.googleapis.com/auth/drive.scripts",
         }
     }
 }
